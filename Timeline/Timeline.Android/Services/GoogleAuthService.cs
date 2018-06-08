60fb1ab26e3cf731b3aa3f5f3edcbb06b2cb0330
bujlaki <balazs.ujlaki@gmail.com>
2018-06-08 17:57:16 +0200

    AWS Cognito

---
+++ Timeline/Timeline.Android/Services/GoogleAuthService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Timeline.Droid.Services
+{
+    class GoogleAuthService : Timeline.Services.IGoogleAuthService
+    {
+        public void Test()
+        {
+            GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+            AccountManager am = AccountManager.get(this);
+            Account[] accounts = am.getAccountsByType(GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+            String token = GoogleAuthUtil.getToken(getApplicationContext(), accounts[0].name,
+                    "audience:server:client_id:YOUR_GOOGLE_CLIENT_ID");
+            Map<String, String> logins = new HashMap<String, String>();
+            logins.put("accounts.google.com", token);
+            credentialsProvider.setLogins(logins);
+        }
+    }
+}
---
+++ Timeline/Timeline/Services/IGoogleAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timeline.Services
+{
+    public interface IGoogleAuthService
+    {
+        void Test();
+    }
+}
