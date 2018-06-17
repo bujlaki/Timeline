6d3d48b0041b0c1768b81d374430b70657a21f3a
Balázs Ujlaki <balazs.ujlaki@gmail.com>
2018-06-17 23:03:21 +0200

    fix colors, add master detail page

---
+++ Timeline/Timeline/Views/VUserPagesMaster.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Timeline.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VUserPagesMaster : ContentPage
+    {
+        public ListView ListView;
+
+        public VUserPagesMaster()
+        {
+            InitializeComponent();
+
+            BindingContext = new VUserPagesMasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class VUserPagesMasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<VUserPagesMenuItem> MenuItems { get; set; }
+            
+            public VUserPagesMasterViewModel()
+            {
+                MenuItems = new ObservableCollection<VUserPagesMenuItem>(new[]
+                {
+                    new VUserPagesMenuItem { Id = 0, Title = "Page 1" },
+                    new VUserPagesMenuItem { Id = 1, Title = "Page 2" },
+                    new VUserPagesMenuItem { Id = 2, Title = "Page 3" },
+                    new VUserPagesMenuItem { Id = 3, Title = "Page 4" },
+                    new VUserPagesMenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ Timeline/Timeline/Views/VUserPagesMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeline.Views
+{
+
+    public class VUserPagesMenuItem
+    {
+        public VUserPagesMenuItem()
+        {
+            TargetType = typeof(VUserPagesDetail);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
