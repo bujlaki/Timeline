973df4e151cf792b0e560986fe02ba4ff459f133
Balázs Ujlaki <balazs.ujlaki@gmail.com>
2018-07-04 23:46:59 +0200

    TLDate

--- Timeline/Timeline/Objects/Date/RollingDateTime.cs
+++ Timeline/Timeline/Objects/Date/RollingDateTime.cs
@@ -1,98 +1,160 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Timeline.Objects.Date
 {
-    class RollingDateTime
+    public class RollingDateTime
     {
-        const Int64 tickspermin = 600000000;
-        const Int64 ticksperhour = 36000000000;
-        const Int64 ticksperday = 864000000000;
-        const int maxyear = 9999;
+        const Int64 TICKS_PER_MINUTE = 600000000;
+        const Int64 TICKS_PER_HOUR  = 36000000000;
+        const Int64 TICKS_PER_DAY   = 864000000000;
+        const int MAX_YEARS = 9999;
 
-        private static void AddMinutes(ref DateTime dt, int count)
+        public DateTime Value { get; set; }
+
+        public RollingDateTime() : this(DateTime.UtcNow) { }
+        public RollingDateTime(DateTime dateTime) { Value = dateTime; }
+
+        protected class RollEventArgs
+        {
+            public int Count { get; set; }
+            public RollEventArgs(int count) { Count = count; }
+        }
+
+        protected delegate void RollEventHandler(RollEventArgs e);
+
+        protected event RollEventHandler OnRollOver;
+
+        public void AddMinutes(int count)
         {
             try
             {
-                dt = dt.AddMinutes(count);
+                Value = Value.AddMinutes(count);
+                DateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                Int64 ticks = dt.Ticks + count * tickspermin;
+                Int64 ticks = Value.Ticks + count * TICKS_PER_MINUTE;
                 while (ticks < 0 || ticks > DateTime.MaxValue.Ticks)
                 {
-                    if (ticks < 0) { ticks += (DateTime.MaxValue.Ticks + 1); }
-                    if (ticks > DateTime.MaxValue.Ticks) { ticks -= (DateTime.MaxValue.Ticks + 1); }
+                    if (ticks < 0) {
+                        ticks += (DateTime.MaxValue.Ticks + 1);
+                        RollOver(-1);
+                    }
+                    if (ticks > DateTime.MaxValue.Ticks) {
+                        ticks -= (DateTime.MaxValue.Ticks + 1);
+                        RollOver(1);
+                    }
                 }
-                dt = new DateTime(ticks);
+                Value = new DateTime(ticks);
+                DateChanged();
             }
         }
 
-        private static void AddHours(ref DateTime dt, int count)
+        public void AddHours(int count)
         {
             try
             {
-                dt = dt.AddHours(count);
+                Value = Value.AddHours(count);
+                DateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                Int64 ticks = dt.Ticks + count * ticksperhour;
+                Int64 ticks = Value.Ticks + count * TICKS_PER_HOUR;
                 while (ticks < 0 || ticks > DateTime.MaxValue.Ticks)
                 {
-                    if (ticks < 0) { ticks += (DateTime.MaxValue.Ticks + 1); }
-                    if (ticks > DateTime.MaxValue.Ticks) { ticks -= (DateTime.MaxValue.Ticks + 1); }
+                    if (ticks < 0) {
+                        ticks += (DateTime.MaxValue.Ticks + 1);
+                        RollOver(-1);
+                    }
+                    if (ticks > DateTime.MaxValue.Ticks) {
+                        ticks -= (DateTime.MaxValue.Ticks + 1);
+                        RollOver(1);
+                    }
                 }
-                dt = new DateTime(ticks);
+                Value = new DateTime(ticks);
+                DateChanged();
             }
         }
 
-        private static void AddDays(ref DateTime dt, int count)
+        public void AddDays(int count)
         {
             try
             {
-                dt = dt.AddDays(count);
+                Value = Value.AddDays(count);
+                DateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                Int64 ticks = dt.Ticks + count * ticksperday;
+                Int64 ticks = Value.Ticks + count * TICKS_PER_DAY;
                 while (ticks < 0 || ticks > DateTime.MaxValue.Ticks)
                 {
-                    if (ticks < 0) { ticks += (DateTime.MaxValue.Ticks + 1); }
-                    if (ticks > DateTime.MaxValue.Ticks) { ticks -= (DateTime.MaxValue.Ticks + 1); }
+                    if (ticks < 0) {
+                        ticks += (DateTime.MaxValue.Ticks + 1);
+                        RollOver(-1);
+                    }
+                    if (ticks > DateTime.MaxValue.Ticks) {
+                        ticks -= (DateTime.MaxValue.Ticks + 1);
+                        RollOver(1);
+                    }
                 }
-                dt = new DateTime(ticks);
+                Value = new DateTime(ticks);
+                DateChanged();
             }
         }
 
-        private static void AddMonths(ref DateTime dt, int count)
+        public void AddMonths(int count)
         {
             int totalToAdd = count;
             try
             {
-                if (Math.Abs(totalToAdd) > 120000) totalToAdd = totalToAdd % 120000;
-                if (totalToAdd < 0) totalToAdd += maxyear * 12;
-                dt = dt.AddMonths(totalToAdd);
+                if (Math.Abs(totalToAdd) > 120000)
+                {
+                    RollOver(totalToAdd / 120000);
+                    totalToAdd = totalToAdd % 120000;
+                }
+                if (totalToAdd < 0)
+                {
+                    totalToAdd += MAX_YEARS * 12;
+                    RollOver(-1);
+                }
+                Value = Value.AddMonths(totalToAdd);
+                DateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                dt = dt.AddMonths(totalToAdd - maxyear * 12);
+                Value = Value.AddMonths(totalToAdd - MAX_YEARS * 12);
+                RollOver(1);
+                DateChanged();
             }
         }
 
-        private static void AddYears(ref DateTime dt, int count)
+        public void AddYears(int count)
         {
             int totalToAdd = count;
             try
             {
-                if (Math.Abs(totalToAdd) > 10000) totalToAdd = totalToAdd % 10000;
-                if (totalToAdd < 0) totalToAdd += maxyear;
-                dt = dt.AddYears(totalToAdd);
+                if (Math.Abs(totalToAdd) > 10000)
+                {
+                    RollOver(totalToAdd / 10000);
+                    totalToAdd = totalToAdd % 10000;
+                }
+                if (totalToAdd < 0)
+                {
+                    totalToAdd += MAX_YEARS;
+                    RollOver(-1);
+                }
+                Value = Value.AddYears(totalToAdd);
+                DateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                dt = dt.AddYears(totalToAdd - maxyear);
+                Value = Value.AddYears(totalToAdd - MAX_YEARS);
+                RollOver(1);
+                DateChanged();
             }
         }
+
+        protected virtual void RollOver(int count) { }
+        protected virtual void DateChanged() { }
     }
 }
---
+++ Timeline/Timeline/Objects/Date/TLDate.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timeline.Objects.Date
+{
+    //DateTime ticks: 
+    //MIN : 0
+    //MAX : 3155378975999999999
+    //1 min : 600.000.000 tick
+    //max minutes (10000 years): 3,155,378,976,000,000,000 / 600,000,000 = 5,258,964,960
+    public enum TLDatePrecision
+    {
+        Minute,
+        Hour,
+        Day,
+        Month,
+        Year,
+        KYear,
+        KKYear,
+        KKKYear,
+        MYear,
+        KMYear,
+        KKMYear,
+        BYear
+    }
+
+    class TLDate : RollingDateTime
+    {
+        
+        private long kkyear;
+        private TLDatePrecision precision;
+
+        public bool AD { get; set; }
+        public Int64 TotalSeconds { get; set; }
+
+        public TLDate(string yearstr)
+        {
+            if (yearstr.ToLower().EndsWith("b"))
+            {
+                yearstr = yearstr.ToLower().Replace("b", "");
+                precision = TLDatePrecision.BYear;
+                kkyear = int.Parse(yearstr) * 100000;
+            }
+            else if (yearstr.ToLower().EndsWith("kkm"))
+            {
+                yearstr = yearstr.ToLower().Replace("kkm", "");
+                precision = TLDatePrecision.KKMYear;
+                kkyear = int.Parse(yearstr) * 10000;
+            }
+            else if (yearstr.ToLower().EndsWith("km"))
+            {
+                yearstr = yearstr.ToLower().Replace("km", "");
+                precision = TLDatePrecision.KMYear;
+                kkyear = int.Parse(yearstr) * 1000;
+            }
+            else if (yearstr.ToLower().EndsWith("m"))
+            {
+                yearstr = yearstr.ToLower().Replace("m", "");
+                precision = TLDatePrecision.MYear;
+                kkyear = int.Parse(yearstr) * 100;
+            }
+            else if (yearstr.ToLower().EndsWith("kkk"))
+            {
+                yearstr = yearstr.ToLower().Replace("kkk", "");
+                precision = TLDatePrecision.KKKYear;
+                kkyear = int.Parse(yearstr) * 10;
+            }
+            else if (yearstr.ToLower().EndsWith("kk"))
+            {
+                yearstr = yearstr.ToLower().Replace("kk", "");
+                precision = TLDatePrecision.KKYear;
+                kkyear = int.Parse(yearstr);
+            }
+        }
+
+        public TLDate(long year, int month = -1, int day = -1, int hour = -1, int minute = -1)
+        {
+            kkyear = year / 10000;
+            year -= (kkyear * 10000);
+
+            if (month == -1)
+            {
+                Value = new DateTime((int)year, 1, 1);
+                precision = TLDatePrecision.Year;
+            }
+            else if (day == -1)
+            {
+                Value = new DateTime((int)year, month, 1);
+                precision = TLDatePrecision.Month;
+            }
+            else if (hour == -1)
+            {
+                Value = new DateTime((int)year, month, day);
+                precision = TLDatePrecision.Day;
+            }
+            else if (minute == -1)
+            {
+                Value = new DateTime((int)year, month, day, hour, 0, 0);
+                precision = TLDatePrecision.Hour;
+            }
+            else
+            {
+                Value = new DateTime((int)year, month, day, hour, minute, 0);
+                precision = TLDatePrecision.Minute;
+            }
+        }
+
+        public TLDate(long kkyear, DateTime dateTime)
+        {
+        }
+
+        protected override void RollOver(int count)
+        {
+            base.RollOver(count);
+        }
+
+        protected override void DateChanged()
+        {
+            base.DateChanged();
+
+
+        }
+    }
+}
