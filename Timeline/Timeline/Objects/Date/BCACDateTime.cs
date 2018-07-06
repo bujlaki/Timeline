aa4c35fe801efd100c19e44cc6700bb2f3ddad40
bujlaki <balazs.ujlaki@gmail.com>
2018-07-06 17:34:06 +0200

    TLDate

--- Timeline/Timeline/Objects/Date/BCACDateTime.cs
+++ Timeline/Timeline/Objects/Date/BCACDateTime.cs
@@ -11,220 +11,138 @@ namespace Timeline.Objects.Date
         }
 
         const Int64 TICKS_PER_MINUTE = 600000000;
-        const Int64 TICKS_PER_HOUR  = 36000000000;
-        const Int64 TICKS_PER_DAY   = 864000000000;
+        const Int64 TICKS_PER_HOUR = 36000000000;
+        const Int64 TICKS_PER_DAY = 864000000000;
         const int MAX_YEARS = 9999;
 
         protected DateTime? bcacDate { get; set; }
         protected BCAC bcac { get; set; }
+        protected int rawYear
+        {
+            get
+            {
+                if (bcacDate == null) return 0;
+                if (bcac == BCAC.AC)
+                    return bcacDate.Value.Year;
+                else
+                    return -10000 + bcacDate.Value.Year;
+            }
+        }
 
         public BCACDateTime() : this(null) { }
         public BCACDateTime(DateTime? dateTime, BCAC bc_or_ac = BCAC.AC)
         {
-            bcacDate = dateTime;
             bcac = bc_or_ac;
+            if (bcac == BCAC.BC)
+                bcacDate = new DateTime(10000 - dateTime.Value.Year, dateTime.Value.Month, dateTime.Value.Day);
+            else
+                bcacDate = dateTime;
         }
 
-        protected class DateChangedArgs
-        {
-            public bool AtMax { get; set; }
-            public bool AtMin { get; set; }
-            public bool BCACChanged { get; set; }
-            public BCAC BcAc { get; set; }
-            public DateChangedArgs(bool isAtMax, bool isAtMin, bool bcacChanged, BCAC bcac)
-            {
-                AtMax = isAtMax;
-                AtMin = isAtMin;
-                BCACChanged = bcacChanged;
-                BcAc = bcac;
-            }
-        }
-
-        public void AddMinutes(int count)
+        public virtual void AddMinutes(int count)
         {
             try
             {
                 bcacDate = bcacDate?.AddMinutes(count);
-                BCACDateChanged(new DateChangedArgs(false, false, false, bcac));
+                BCACDateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                if (bcac==BCAC.AC && count>0)
-                {
-                    bcacDate = DateTime.MaxValue;
-                    BCACDateChanged(new DateChangedArgs(true, false, false, bcac));
-                }
-                else if (bcac==BCAC.BC && count < 0)
-                {
-                    bcacDate = DateTime.MinValue;
-                    BCACDateChanged(new DateChangedArgs(false, true, false, bcac));
-                }
-                else
-                {
-                    Int64 ticks = bcacDate.Value.Ticks + count * TICKS_PER_MINUTE;
-                    while (ticks < 0 || ticks > DateTime.MaxValue.Ticks)
-                    {
-                        if (ticks < 0) ticks += (DateTime.MaxValue.Ticks + 1);
-                        if (ticks > DateTime.MaxValue.Ticks) ticks -= (DateTime.MaxValue.Ticks + 1);
-                    }
-                    bcacDate = new DateTime(ticks);
-                    BCACDateChanged(new DateChangedArgs(false, false, true, bcac));
-                }
+                if (bcac == BCAC.AC && count > 0) throw new OverflowException();
+                if (bcac == BCAC.BC && count < 0) throw new OverflowException();
 
+                Int64 ticks = bcacDate.Value.Ticks + count * TICKS_PER_MINUTE;
+                if (ticks < 0) ticks += (DateTime.MaxValue.Ticks + 1);
+                if (ticks > DateTime.MaxValue.Ticks) ticks -= (DateTime.MaxValue.Ticks + 1);
+                bcac = count > 0 ? BCAC.AC : BCAC.BC;
+                bcacDate = new DateTime(ticks);
+                BCACDateChanged();
             }
         }
 
-        public void AddHours(int count)
+        public virtual void AddHours(int count)
         {
             try
             {
                 bcacDate = bcacDate?.AddHours(count);
-                BCACDateChanged(new DateChangedArgs(false, false, false, bcac));
+                BCACDateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                if (bcac == BCAC.AC && count > 0)
-                {
-                    bcacDate = DateTime.MaxValue;
-                    BCACDateChanged(new DateChangedArgs(true, false, false, bcac));
-                }
-                else if (bcac == BCAC.BC && count < 0)
-                {
-                    bcacDate = DateTime.MinValue;
-                    BCACDateChanged(new DateChangedArgs(false, true, false, bcac));
-                }
-                else
-                {
-                    Int64 ticks = bcacDate.Value.Ticks + count * TICKS_PER_HOUR;
-                    while (ticks < 0 || ticks > DateTime.MaxValue.Ticks)
-                    {
-                        if (ticks < 0)
-                        {
-                            ticks += (DateTime.MaxValue.Ticks + 1);
-                        }
-                        if (ticks > DateTime.MaxValue.Ticks)
-                        {
-                            ticks -= (DateTime.MaxValue.Ticks + 1);
-                        }
-                    }
-                    bcacDate = new DateTime(ticks);
-                    BCACDateChanged(new DateChangedArgs(false, false, true, bcac));
-                }
+                if (bcac == BCAC.AC && count > 0) throw new OverflowException();
+                if (bcac == BCAC.BC && count < 0) throw new OverflowException();
+
+                Int64 ticks = bcacDate.Value.Ticks + count * TICKS_PER_HOUR;
+                if (ticks < 0) ticks += (DateTime.MaxValue.Ticks + 1);
+                if (ticks > DateTime.MaxValue.Ticks) ticks -= (DateTime.MaxValue.Ticks + 1);
+                bcac = count > 0 ? BCAC.AC : BCAC.BC;
+                bcacDate = new DateTime(ticks);
+                BCACDateChanged();
             }
         }
 
-        public void AddDays(int count)
+        public virtual void AddDays(int count)
         {
             try
             {
                 bcacDate = bcacDate?.AddDays(count);
-                BCACDateChanged(new DateChangedArgs(false, false, false, bcac));
+                BCACDateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                if (bcac == BCAC.AC && count > 0)
-                {
-                    bcacDate = DateTime.MaxValue;
-                    BCACDateChanged(new DateChangedArgs(true, false, false, bcac));
-                }
-                else if (bcac == BCAC.BC && count < 0)
-                {
-                    bcacDate = DateTime.MinValue;
-                    BCACDateChanged(new DateChangedArgs(false, true, false, bcac));
-                }
-                else
-                {
-                    Int64 ticks = bcacDate.Value.Ticks + count * TICKS_PER_DAY;
-                    while (ticks < 0 || ticks > DateTime.MaxValue.Ticks)
-                    {
-                        if (ticks < 0)
-                        {
-                            ticks += (DateTime.MaxValue.Ticks + 1);
-                        }
-                        if (ticks > DateTime.MaxValue.Ticks)
-                        {
-                            ticks -= (DateTime.MaxValue.Ticks + 1);
-                        }
-                    }
-                    bcacDate = new DateTime(ticks);
-                    BCACDateChanged(new DateChangedArgs(false, false, true, bcac));
-                }
+                if (bcac == BCAC.AC && count > 0) throw new OverflowException();
+                if (bcac == BCAC.BC && count < 0) throw new OverflowException();
+
+                Int64 ticks = bcacDate.Value.Ticks + count * TICKS_PER_DAY;
+                if (ticks < 0) ticks += (DateTime.MaxValue.Ticks + 1);
+                if (ticks > DateTime.MaxValue.Ticks) ticks -= (DateTime.MaxValue.Ticks + 1);
+                bcac = count > 0 ? BCAC.AC : BCAC.BC;
+                bcacDate = new DateTime(ticks);
+                BCACDateChanged();
             }
         }
 
-        public void AddMonths(int count)
+        public virtual void AddMonths(int count)
         {
-            int totalToAdd = count;
             try
             {
-                if (Math.Abs(totalToAdd) > 120000)
-                {
-                    totalToAdd = totalToAdd % 120000;
-                }
-                if (totalToAdd < 0)
-                {
-                    totalToAdd += MAX_YEARS * 12;
-                }
-                bcacDate = bcacDate?.AddMonths(totalToAdd);
-                BCACDateChanged(new DateChangedArgs(false, false, false, bcac));
+                bcacDate = bcacDate?.AddMonths(count);
+                BCACDateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                if (bcac == BCAC.AC && count > 0)
-                {
-                    bcacDate = DateTime.MaxValue;
-                    BCACDateChanged(new DateChangedArgs(true, false, false, bcac));
-                }
-                else if (bcac == BCAC.BC && count < 0)
-                {
-                    bcacDate = DateTime.MinValue;
-                    BCACDateChanged(new DateChangedArgs(false, true, false, bcac));
-                }
-                else
-                {
-                    bcacDate = bcacDate?.AddMonths(totalToAdd - MAX_YEARS * 12);
-                    BCACDateChanged(new DateChangedArgs(false, false, true, bcac));
-                }
+                if (bcac == BCAC.AC && count > 0) throw new OverflowException();
+                if (bcac == BCAC.BC && count < 0) throw new OverflowException();
+
+                bcac = count > 0 ? BCAC.AC : BCAC.BC;
+                if (count > 0) bcacDate = bcacDate?.AddMonths(count - MAX_YEARS * 12);
+                if (count < 0) bcacDate = bcacDate?.AddMonths(MAX_YEARS * 12 + count);
+                BCACDateChanged();
             }
         }
 
-        public void AddYears(int count)
+        public virtual void AddYears(int count)
         {
-            int totalToAdd = count;
             try
             {
-                if (Math.Abs(totalToAdd) > 10000)
-                {
-                    totalToAdd = totalToAdd % 10000;
-                }
-                if (totalToAdd < 0)
-                {
-                    totalToAdd += MAX_YEARS;
-                }
-                bcacDate = bcacDate?.AddYears(totalToAdd);
-                BCACDateChanged(new DateChangedArgs(false, false, false, bcac));
+                bcacDate = bcacDate?.AddYears(count);
+                BCACDateChanged();
             }
             catch (ArgumentOutOfRangeException)
             {
-                if (bcac == BCAC.AC && count > 0)
-                {
-                    bcacDate = DateTime.MaxValue;
-                    BCACDateChanged(new DateChangedArgs(true, false, false, bcac));
-                }
-                else if (bcac == BCAC.BC && count < 0)
-                {
-                    bcacDate = DateTime.MinValue;
-                    BCACDateChanged(new DateChangedArgs(false, true, false, bcac));
-                }
-                else
-                {
-                    bcacDate = bcacDate?.AddYears(totalToAdd - MAX_YEARS);
-                    BCACDateChanged(new DateChangedArgs(false, false, true, bcac));
-                }
+                if (bcac == BCAC.AC && count > 0) throw new OverflowException();
+                if (bcac == BCAC.BC && count >= 2 * MAX_YEARS - bcacDate.Value.Year) throw new OverflowException();
+                if (bcac == BCAC.BC && count < 0) throw new OverflowException();
+                if (bcac == BCAC.AC && count <= -MAX_YEARS - bcacDate.Value.Year) throw new OverflowException();
+
+                bcac = count > 0 ? BCAC.AC : BCAC.BC;
+                if (count > 0) bcacDate = bcacDate?.AddYears(count - MAX_YEARS);
+                if (count < 0) bcacDate = bcacDate?.AddYears(MAX_YEARS + count);
+                BCACDateChanged();
             }
         }
 
-        protected virtual void BCACDateChanged(DateChangedArgs args) { }
+        protected virtual void BCACDateChanged() { }
 
     }
 }
--- Timeline/Timeline/Objects/Date/TLDate.cs
+++ Timeline/Timeline/Objects/Date/TLDate.cs
@@ -23,23 +23,34 @@ namespace Timeline.Objects.Date
         BYear
     }
 
-    class TLDate : BCACDateTime
+    public class TLDate : BCACDateTime
     {
         private const TLDatePrecision PRECISION_MIN = TLDatePrecision.Minute;
         private const TLDatePrecision PRECISION_MAX = TLDatePrecision.BYear;
+        private const float MINUTES_PER_YEAR = 525949.20f;
+        private const long MINUTES_PER_KKYEAR = 5258964959;
+        private const long TICKS_PER_MINUTE = 600000000;
         private const long MAX_YEAR = 99000000000;
 
         private Int64 exactYear;
         private TLDatePrecision precision;
 
-        public bool AD { get; set; }
-        public Int64 TotalSeconds { get; set; }
+        public bool AD { get { return bcac == BCAC.AC; } }
+        public Int64 TotalMinutes { get; private set; }
 
         public Int64 Year
         {
             get
             {
-                return exactYear;
+                if (bcacDate == null)
+                {
+                    return exactYear;
+                }
+                else
+                {
+                    if (bcac == BCAC.BC) return 10000 - bcacDate.Value.Year;
+                    return bcacDate.Value.Year;
+                }
             }
             set
             {
@@ -47,48 +58,61 @@ namespace Timeline.Objects.Date
                 bcac = exactYear < 0 ? BCAC.BC : BCAC.AC;
             }
         }
+        public string YearStr
+        {
+            get
+            {
+                return (bcac == BCAC.BC) ? "BC. " + Year.ToString() : "AC. " + Year.ToString();
+            }
+        }
+        public string PrecisionStr
+        {
+            get
+            {
+                switch (precision)
+                {
+                    case TLDatePrecision.Minute: return "Minute";
+                    case TLDatePrecision.Hour: return "Hour";
+                    case TLDatePrecision.Day: return "Day";
+                    case TLDatePrecision.Month: return "Month";
+                    case TLDatePrecision.Year: return "Year";
+                    case TLDatePrecision.KYear: return "KYear";
+                    case TLDatePrecision.KKYear: return "KKYear";
+                    case TLDatePrecision.KKKYear: return "KKKYear";
+                    case TLDatePrecision.MYear: return "MYear";
+                    case TLDatePrecision.KMYear: return "KMYear";
+                    case TLDatePrecision.KKMYear: return "KKMYear";
+                    case TLDatePrecision.BYear: return "BYear";
+                    default: return "";
+                }
+            }
+        }
+        public string DateStrFull
+        {
+            get
+            {
+                string ret = YearStr;
+                if (bcacDate != null)
+                {
+                    ret = ret + "." + bcacDate.Value.Month.ToString() + "." + bcacDate.Value.Day.ToString() + ". ";
+                    ret = ret + bcacDate.Value.Hour.ToString() + ":" + bcacDate.Value.Minute.ToString();
+                }
+                return ret;
+            }
+        }
+
         public TLDate(string yearstr)
         {
-            if (yearstr.ToLower().EndsWith("b"))
+            if (yearstr.ToLower().EndsWith("b"))        //billion years
             {
-                //billion years
                 yearstr = yearstr.ToLower().Replace("b", "");
                 precision = TLDatePrecision.BYear;
-                bcacDate = null;
-
-                Year = int.Parse(yearstr) * MultiplierForPrecision(precision);
-                if (Math.Abs(exactYear) > MAX_YEAR)
-                {
-                    exactYear = 0;
-                    throw new OverflowException("Maximum 99 billion years");
-                }
+                exactYear = int.Parse(yearstr) * MultiplierForPrecision(precision);
             }
-            else if (yearstr.ToLower().EndsWith("m"))
+            else if (yearstr.ToLower().EndsWith("m"))   //million years
             {
-                //million years
                 yearstr = yearstr.ToLower().Replace("m", "");
                 precision = TLDatePrecision.MYear;
-                bcacDate = null;
-
-                while (yearstr.EndsWith("0") && precision < PRECISION_MAX) {
-                    precision += 1;
-                    yearstr = yearstr.Substring(0, yearstr.Length - 1);
-                    if (String.IsNullOrEmpty(yearstr)) throw new ArgumentException("Incorrect value");
-                }
-
-                Year = int.Parse(yearstr) * MultiplierForPrecision(precision);
-                if (Math.Abs(exactYear) > MAX_YEAR)
-                {
-                    exactYear = 0;
-                    throw new OverflowException("Maximum 99 billion years");
-                }
-            }
-            else if (yearstr.ToLower().EndsWith("k"))
-            {
-                //thousand years
-                yearstr = yearstr.ToLower().Replace("k", ""); 
-                precision = TLDatePrecision.KYear;
-                bcacDate = null;
 
                 while (yearstr.EndsWith("0") && precision < PRECISION_MAX)
                 {
@@ -97,75 +121,87 @@ namespace Timeline.Objects.Date
                     if (String.IsNullOrEmpty(yearstr)) throw new ArgumentException("Incorrect value");
                 }
 
-                Year = long.Parse(yearstr) * MultiplierForPrecision(precision);
-                if (Math.Abs(exactYear) > MAX_YEAR)
+                exactYear = int.Parse(yearstr) * MultiplierForPrecision(precision);
+            }
+            else if (yearstr.ToLower().EndsWith("k"))   //thousand years
+            {
+                yearstr = yearstr.ToLower().Replace("k", "");
+                precision = TLDatePrecision.KYear;
+
+                while (yearstr.EndsWith("0") && precision < PRECISION_MAX)
                 {
-                    exactYear = 0;
-                    throw new OverflowException("Maximum 99 billion years");
+                    precision += 1;
+                    yearstr = yearstr.Substring(0, yearstr.Length - 1);
+                    if (String.IsNullOrEmpty(yearstr)) throw new ArgumentException("Incorrect value");
                 }
 
-                if (exactYear < 10000)
-                {
-                    initBCACDate((int)exactYear, -1, -1, -1, -1, false);
-                }
+                exactYear = long.Parse(yearstr) * MultiplierForPrecision(precision);
             }
 
-        }
+            if (Math.Abs(exactYear) > MAX_YEAR) throw new OverflowException("Maximum 99 billion years");
 
-        public TLDate(int year, int month = -1, int day = -1, int hour = -1, int minute = -1)
-        {
-            initBCACDate(year, month, day, hour, minute);
-        }
-
-        public TLDate(Int64 year)
-        {
-            if(Math.Abs(year)<10000)
+            bcacDate = null;
+            if (Math.Abs(exactYear) < 10000)
             {
-                initBCACDate((int)year);
+                bcac = exactYear < 0 ? BCAC.BC : BCAC.AC;
+                Initialize((int)exactYear);
+            }
+            BCACDateChanged();
+        }
+
+        public TLDate(Int64 year, int month = -1, int day = -1, int hour = -1, int minute = -1)
+        {
+            bcac = year < 0 ? BCAC.BC : BCAC.AC;
+            if (Math.Abs(year) < 10000)
+            {
+                exactYear = year;
+                Initialize((int)year, month, day, hour, minute);
             }
             else
             {
                 bcacDate = null;
                 precision = TLDatePrecision.Year;
-                if (year<0) {
-                    exactYear = -year;
-                    bcac = BCAC.BC;
-                }
-                else {
-                    exactYear = year;
-                    bcac = BCAC.AC;
-                }
+                exactYear = year; // year < 0 ? -year : year;
+            }
+            BCACDateChanged();
+        }
+
+        public static TLDate FromTotalMinutes(Int64 minutes)
+        {
+            if (minutes > MINUTES_PER_KKYEAR)
+            {
+
             }
         }
 
-        private void initBCACDate(int year, int month = -1, int day = -1, int hour = -1, int minute = -1, bool setPrecision = true)
+        private void Initialize(int year, int month = -1, int day = -1, int hour = -1, int minute = -1)
         {
-            year = year % 10000;
+            if (year < 0) year = year + 10000;
 
             if (month == -1)
             {
                 bcacDate = new DateTime(year, 1, 1);
-                if(setPrecision) precision = TLDatePrecision.Year;
+                precision = TLDatePrecision.Year;
             }
             else if (day == -1)
             {
                 bcacDate = new DateTime(year, month, 1);
-                if (setPrecision) precision = TLDatePrecision.Month;
+                precision = TLDatePrecision.Month;
             }
             else if (hour == -1)
             {
                 bcacDate = new DateTime(year, month, day);
-                if (setPrecision) precision = TLDatePrecision.Day;
+                precision = TLDatePrecision.Day;
             }
             else if (minute == -1)
             {
                 bcacDate = new DateTime(year, month, day, hour, 0, 0);
-                if (setPrecision) precision = TLDatePrecision.Hour;
+                precision = TLDatePrecision.Hour;
             }
             else
             {
                 bcacDate = new DateTime(year, month, day, hour, minute, 0);
-                if (setPrecision) precision = TLDatePrecision.Minute;
+                precision = TLDatePrecision.Minute;
             }
         }
 
@@ -173,6 +209,7 @@ namespace Timeline.Objects.Date
         {
             switch (precision)
             {
+                case TLDatePrecision.KYear: return 1000;
                 case TLDatePrecision.KKYear: return 10000;
                 case TLDatePrecision.KKKYear: return 100000;
                 case TLDatePrecision.MYear: return 1000000;
@@ -183,10 +220,79 @@ namespace Timeline.Objects.Date
             }
         }
 
-        protected override void BCACDateChanged(DateChangedArgs args)
+        public override void AddMinutes(int count)
         {
-            
-            base.BCACDateChanged(args);
+            try { base.AddMinutes(count); }
+            catch (OverflowException)
+            {
+                exactYear = count > 0 ? 10000 : -10000;
+                bcacDate = null;
+                BCACDateChanged();
+            }
+        }
+
+        public override void AddHours(int count)
+        {
+            try { base.AddHours(count); }
+            catch (OverflowException)
+            {
+                exactYear = count > 0 ? 10000 : -10000;
+                bcacDate = null;
+                BCACDateChanged();
+            }
+        }
+
+        public override void AddDays(int count)
+        {
+            try { base.AddDays(count); }
+            catch (OverflowException)
+            {
+                exactYear = count > 0 ? 10000 : -10000;
+                bcacDate = null;
+                BCACDateChanged();
+            }
+        }
+
+        public override void AddMonths(int count)
+        {
+            try { base.AddMonths(count); }
+            catch (OverflowException)
+            {
+                exactYear = rawYear + (count / 12);
+                if (count > 0)
+                    if (bcacDate.Value.Month + (count % 12) > 12) exactYear++;
+                if (count < 0)
+                    if (bcacDate.Value.Month + (count % 12) < 1) exactYear--;
+
+                bcacDate = null;
+                BCACDateChanged();
+            }
+        }
+
+        public override void AddYears(int count)
+        {
+            try { base.AddYears(count); }
+            catch (OverflowException)
+            {
+                exactYear = exactYear + count;
+                bcacDate = null;
+                BCACDateChanged();
+            }
+        }
+
+        protected override void BCACDateChanged()
+        {
+            if (bcacDate == null)
+            {
+                TotalMinutes = (Int64)(exactYear * MINUTES_PER_YEAR);
+            }
+            else
+            {
+                if (bcac == BCAC.BC)
+                    TotalMinutes = (bcacDate.Value.Ticks - (DateTime.MaxValue.Ticks + 1)) / TICKS_PER_MINUTE;
+                else
+                    TotalMinutes = bcacDate.Value.Ticks / TICKS_PER_MINUTE;
+            }
         }
     }
 }
